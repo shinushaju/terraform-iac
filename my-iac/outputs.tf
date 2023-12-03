3cfd1b48cb6dd97ddaabb841130c7a4045661d5e
shinushaju <shinushaju.in@gmail.com>
2023-12-03 03:51:47 -0800

    new: add ec2-instance module outputs

--- my-iac/outputs.tf
+++ my-iac/outputs.tf
@@ -12,3 +12,43 @@ output "vpc_cidr_block" {
   description = "The IPv4 CIDR block for the VPC."
   value       = module.vpc.vpc_cidr_block
 }
+
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance."
+  value       = module.ec2-instance.id
+}
+
+output "ec2_instance_arn" {
+  description = "The ARN of the EC2 instance."
+  value       = module.ec2-instance.arn
+}
+
+output "ec2_instance_state" {
+  description = "The state of the EC2 instance."
+  value       = module.ec2-instance.instance_state
+}
+
+output "ec2_instance_key_name" {
+  description = "The AWS key pair that was used to create the EC2 instance."
+  value       = module.ec2-instance.key_name
+}
+
+output "ec2_instance_ami" {
+  description = "The AMI ID that was used to create the EC2 instance."
+  value       = module.ec2-instance.ami
+}
+
+output "ec2_instance_availability_zone" {
+  description = "The availability zone of the created EC2 instance"
+  value       = module.ec2-instance.availability_zone
+}
+
+output "ec2_instance_public_ip" {
+  description = "The public IP address assigned to the EC2 instance."
+  value       = module.ec2-instance.public_ip
+}
+
+output "ec2_instance_private_ip" {
+  description = "The private IP address assigned to the EC2 instance."
+  value       = module.ec2-instance.private_ip
+}
